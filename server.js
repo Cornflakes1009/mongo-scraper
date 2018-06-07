9bae237059ad5ad0014ade639ee8bf963da2049e
Harold Davidson <harold.davidson89@gmail.com>
2018-06-07 12:57:34 -0500

    added port config and mLab

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-// port configuration for local/Heroku
-var PORT = process.env.PORT || process.argv[2] || 8080;
-
 var app = express();
+
+// port configuration for local/Heroku
+// var PORT = process.env.PORT || process.argv[2] || 8080;
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-scraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,6 +42,6 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-});
+// app.listen(PORT, function () {
+//     console.log("App running on port " + PORT + "!");
+// });
